49e2e456836fac7265e9626e9bd50a7c6716b495
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-02-08 11:47:22 +0530

    react

--- google-sheet-react-app/src/components/SheetForm.js
+++ google-sheet-react-app/src/components/SheetForm.js
@@ -55,57 +55,6 @@ const SheetForm = () => {
     }
   };
 
-  //   const handleSubmit = (event) => {
-  //     event.preventDefault();
-  //     updateSheetValues;
-  //   };
-  // const updateSheetValues = (event) => {
-  //   event.preventDefault();
-  //   const data = Object.values(inputs).join(",");
-  //   console.log(data);
-  //   const response = fetch(
-  //     `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`,
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         //update this token with yours.
-  //         Authorization: `Bearer ${ACCESS_TOKEN}`,
-  //       },
-  //       body: JSON.stringify({
-  //         requests: [
-  //           {
-  //             insertRange: {
-  //               range: {
-  //                 sheetId: 1234567890,
-  //                 startRowIndex: 32,
-  //                 endRowIndex: 33,
-  //               },
-  //               shiftDimension: "ROWS",
-  //             },
-  //           },
-  //           {
-  //             pasteData: {
-  //               data: data,
-  //               type: "PASTE_NORMAL",
-  //               delimiter: ",",
-  //               coordinate: {
-  //                 sheetId: 0,
-  //                 rowIndex: 32,
-  //               },
-  //             },
-  //           },
-  //         ],
-  //       }),
-  //     }
-  //   );
-  //   if (!response.ok) {
-  //     console.log("response", response);
-  //     throw new Error("Data coud not be append!");
-  //   } else {
-  //     return response.json();
-  //   }
-  // };
   return (
     <form onSubmit={updateSheetValues}>
       <label htmlFor="Student Name">Student name: </label>
